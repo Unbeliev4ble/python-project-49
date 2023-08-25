dc413874f2cab77bd73146f38751e162da41edd3
Andrey Volkov <andrew.unbelievable@yandex.ru>
2023-08-25 07:28:22 +0300

    change bracket pos in calc_logic

--- brain_games/games/bg_calc_logic.py
+++ brain_games/games/bg_calc_logic.py
@@ -11,11 +11,13 @@ def game_options():
         '+': operator.add,
         '-': operator.sub,
         '*': operator.mul,
-        }
+    }
+
     picked_operator = random.choice(operator_list)
 
     num_1 = random.randint(1, 10)
     num_2 = random.randint(1, 12)
+
     expression = f"{num_1} {picked_operator} {num_2}"
     correct_answer = str(operator_functions[picked_operator](num_1, num_2))
     return expression, correct_answer
